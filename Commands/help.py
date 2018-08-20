526d87ebeaa79cd1b60bc532c83e10fbe957897a
Cullen Brown <cjb3768@gmail.com>
2018-08-20 16:03:11 -0500

    Laying framework for function specific help documentation for the !help command

--- Commands/help.py
+++ Commands/help.py
@@ -1,6 +1,14 @@
 import discord
 import asyncio
 
-async def execute(client, message, args):
-    """Send user a private message with a list of all supported functions"""
-    await client.send_message(message.author, "This is a help message")
+async def execute(client, message, args, modules):
+    """Send user a private message with a list of all supported functions or the key functionality of a given command"""
+
+    if args == "":
+        await client.send_message(message.author, "List of commands")
+
+    else:
+        try:
+            await client.send_message(message.author, "'{}': {}".format(args, modules[args].execute.__doc__))
+        except KeyError:
+            print("Unknown module '{}'!".format(args))
