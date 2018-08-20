10f07cef7e97fad58edc1ed736e2806bedb5276b
Cullen Brown <cjb3768@gmail.com>
2018-08-20 12:59:57 -0500

    Simple Messaging Support for Bot
    Have rudimentary support for sending messages to the bot, and wrote a rudimentary function to ask the bot for a connection url (will be moved to a separate function later)

---
+++ test.py
@@ -0,0 +1,39 @@
+import discord
+import logging
+import asyncio
+
+logging.basicConfig(level=logging.INFO)
+
+#this next function could be used later to spread the bot; can be moved later
+def generate_oauth_url(client_id, permissions):
+    """Generate the bot's OAuth2 URL to add it to a server from a given client_id and set of permissions"""
+    return "https://discordapp.com/api/oauth2/authorize?client_id={}&permissions={}&scope=bot".format(client_id, permissions)
+
+#read in token info from bot_token.txt
+file = open("bot_token.txt","r")
+token = file.read()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('-------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculate messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run(token)
