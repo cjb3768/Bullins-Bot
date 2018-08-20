8e7ddaca73ee9674859392035a354f073eefb519
Matt <7395051@gmail.com>
2018-08-20 15:47:33 -0500

    Got automatic module loading proof-of-concept working. shows how to load modules and access their docstring.

--- Commands/echo.py
+++ Commands/echo.py
@@ -1,7 +1,7 @@
 import discord
 import asyncio
 
-async def echo (client, message, args):
+async def execute(client, message, args):
     """Copy user message after the echo statement"""
     #print("Echoing message")
     await client.send_message(message.channel, args)
