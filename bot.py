4bf5f5092ac90f584f81fe563486a16f754d44b4
Matt <7395051@gmail.com>
2018-08-21 12:41:22 -0500

    Invocations are now consumed entirely, rather than ignored. Formalized a comment format. Added more descriptive comments. Added fixme.

--- bot.py
+++ bot.py
@@ -4,39 +4,51 @@ import logging
 import pkgutil
 import Commands
 
+###########
+# GLOBALS #
+###########
 logger = logging.getLogger("bullinsbot")
 client = discord.Client()
-invocation = "b! "
 modules = {}
+
+invocation = "b! "
+
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('-------')
+    logger.info("Logged in as '%s', ID: %s", client.user.name, client.user.id)
 
-    #TODO: I Think the Introduction mught go here
+    #TODO: I Think an introduction might go here.
+
 
 @client.event
 async def on_message(message):
+    # Check if incoming message is intended for this bot.
     if message.content.startswith(invocation):
+        # Consume the invocation.
+        message.content = message.content[len(invocation):]
+
+        # Seperate the command from any additional arguments.
         try:
-            _, command, args = message.content.split(' ', 2)
+            command, args = message.content.split(' ', 1)
         except ValueError:
-            _, command, args = message.content.split(' ', 1), ''
+            command, args = message.content, ''
 
         logger.info("Given command '%s' with args '%s'", command, args)
 
         try:
+            # Attempt to execute the given command.
             await modules[command].execute(client, message, args, modules)
         except KeyError:
             logger.error("Unrecognized command: %s", command)
-            #respond that the command is unrecognized and suggest checking 'help'
+            #FIXME: Respond that the command is unrecognized and suggest checking 'help'
+
 
 def main():
+    # Set logging to output all messages INFO level or higher.
     logging.basicConfig(level=logging.INFO)
 
+    # Import all modules from the Commands package and categorize them.
     prefix = Commands.__name__ + "."
     for importer, mod_name, ispkg in pkgutil.iter_modules(Commands.__path__, prefix):
         sub_mod_name = mod_name.split(".", 1)[-1]
@@ -44,11 +56,13 @@ def main():
 
     logger.info("Found %s command modules.", len(modules))
 
-    #read in token info from bot_token.txt
+    # Read in token info from bot_token.txt
     token_file = open("bot_token.txt","r")
     bot_login_token = token_file.read()
 
+    # Start the client and give it our token.
     client.run(bot_login_token)
+
 
 if __name__ == "__main__":
     main()
