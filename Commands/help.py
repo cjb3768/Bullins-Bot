552877b9ee394b2e50e3e08fe508bc847197af0c
Matt <7395051@gmail.com>
2018-08-21 11:45:19 -0500

    Rudimentary functionality acquired
    Still need to clean up and add more descriptive comments
    rewrite strings to be more descriptive and less casual.

--- Commands/help.py
+++ Commands/help.py
@@ -1,14 +1,27 @@
 import discord
 import asyncio
+import logging
+
+logger = logging.getLogger("bullinsbot.help")
 
 async def execute(client, message, args, modules):
     """Send user a private message with a list of all supported functions or the key functionality of a given command"""
 
-    if args == "":
-        await client.send_message(message.author, "List of commands")
+    if args:
+        logger.debug("looking for spicific info about command %s.", args)
 
-    else:
         try:
             await client.send_message(message.author, "'{}': {}".format(args, modules[args].execute.__doc__))
         except KeyError:
-            print("Unknown module '{}'!".format(args))
+            logger.warning("Unknown command '%s'!", args)
+
+    else:
+        logger.debug("looking for general help.")
+
+        module_list = "\n"
+        for command in modules:
+            module_list += command + "\n"
+
+        helptext = "Supports the following commands:{}Type `!help [command]` for more information.".format(module_list)
+
+        await client.send_message(message.author, helptext)
