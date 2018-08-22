2398b9ec51bf73f199245785bfebda67f8c981cd
Cullen Brown <cjb3768@gmail.com>
2018-08-22 16:05:44 -0500

    Clean working, now purges a number of bot-related messages from a given chat.

--- Commands/clean.py
+++ Commands/clean.py
@@ -4,14 +4,11 @@ import logging
 
 logger = logging.getLogger("bullinsbot.clean")
 
-def is_bot_response(client, message):
-    return message.author == client.user
-
-def is_bot_request(client, message):
-    return message.content.startswith(client.invocation)
-
 async def execute(client, message, args, _):
-    """Deletes all past bot commands and replies from requested channel"""
-    async for log in client.logs_from(message.channel):
-        if is_bot_request(client, log) or is_bot_response(client, log):
-            await client.delete_message(log)
+    """Deletes a number of past bot commands and replies from requested channel"""
+    def bot_related(m):
+        return m.content.startswith(client.invocation) or m.author == client.user
+
+    deleted_messages = await client.purge_from(message.channel, limit=int(args), check=bot_related)
+
+    logger.info("Deleted %d bot-related messages from channel.", len(deleted_messages))
