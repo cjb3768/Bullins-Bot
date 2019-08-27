1d2eb56ec7151ac89e10a0c1ffb9a5b03ff982d1
Cullen Brown <cjb3768@gmail.com>
2019-08-27 16:15:18 -0500

    Fixed echo.py Just needed to change that single line of code, handing over message sending from the client to the channel itself.

--- Commands/echo.py
+++ Commands/echo.py
@@ -15,4 +15,4 @@ def get_available_commands():
 async def execute(client, message, instruction, **kwargs):
     """Copy user message after the echo statement"""
     logger.debug("Echoing message: %s", ' '.join(instruction[1:]))
-    await client.send_message(message.channel, ' '.join(instruction[1:]))
+    await message.channel.send(' '.join(instruction[1:]))
